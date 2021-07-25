a15a82f20c9c468b214e1ba9410d5ecbbd7a8361
Katerynaru4 <70796523+Katerynaru4@users.noreply.github.com>
2021-07-25 20:03:07 +0300

    Finding multiple items on a page

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-export function getTitleElement() {
-  const titleElem = document.querySelector('.title');
-  console.dir(titleElem);
-  return titleElem;
-}
+export const getItemsList = () => {
+  const elementsList = document.querySelectorAll('.technology');
+  console.dir(elementsList);
+  return elementsList;
+};
+export const getItemsArray = () => {
+  const elementsArray = Array.from(document.querySelectorAll('.tool'));
+  console.dir(elementsArray);
+  return elementsArray;
+};
 
-export function getInputElement() {
-  const inputElem = document.querySelector('[type="text"]');
-  console.dir(inputElem);
-  return inputElem;
-}
