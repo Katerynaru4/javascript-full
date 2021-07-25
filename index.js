f1b29489935eb09de8d7c25929bf3ebfecdf7abd
Katerynaru4 <70796523+Katerynaru4@users.noreply.github.com>
2021-07-25 20:15:38 +0300

    Update text content

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-export const getTitle = () => {
+export const setTitle = (text) => {
   const title = document.querySelector('.title');
-  return title.textContent;
-};
-export const getDescription = () => {
-  const about = document.querySelector('.about');
-  return about.innerText;
-};
-export const getPlans = () => {
-  const plans = document.querySelector('.plans');
-  return plans.innerHTML;
-};
-export const getGoal = () => {
-  const goal = document.querySelector('.goal');
-  return goal.outerHTML;
+  title.textContent = text;
 };
