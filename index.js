ae876668222cab221239ef820d539dc54a33b21b
Katerynaru4 <70796523+Katerynaru4@users.noreply.github.com>
2021-07-25 21:06:21 +0300

    Creating DOM elements and inserting into the page

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-export const createButton = (buttonText) => {
-  const button = document.createElement('button');
-  button.textContent = buttonText;
-  const body = document.querySelector('body');
-  body.append(button);
+export const finishList = () => {
+  const list = document.querySelector('.list');
+  const special = document.querySelector('.special');
+
+  const listItem1 = document.createElement('li');
+  listItem1.textContent = '1';
+  const listItem4 = document.createElement('li');
+  listItem4.textContent = '4';
+  const listItem6 = document.createElement('li');
+  listItem6.textContent = '6';
+  const listItem8 = document.createElement('li');
+  listItem8.textContent = '8';
+
+  list.prepend(listItem1);
+  list.append(listItem8);
+  special.before(listItem4);
+  special.after(listItem6);
 };
