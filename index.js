381d2e42c7a03a50059be6c71aa9e3f7a5f346c4
Katerynaru4 <70796523+Katerynaru4@users.noreply.github.com>
2021-07-25 21:49:34 +0300

    classes

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-export const finishForm = () => {
-  const loginForm = document.querySelector('.login-form');
-  const nameInput = document.createElement('input');
-  nameInput.setAttribute('type', 'text');
-  nameInput.setAttribute('name', 'login');
-  loginForm.prepend(nameInput);
-  const passwordInput = document.querySelector('[name="password"]');
-  passwordInput.setAttribute('type', 'password');
+export const manageClasses = () => {
+  const one = document.querySelector('.one');
+  const two = document.querySelector('.two');
+  const three = document.querySelector('.three');
+  const four = document.querySelector('.four');
+
+  one.classList.add('selected');
+  two.classList.remove('selected');
+  three.classList.toggle('three_done');
+  if (four.classList.contains('some-class')) {
+    four.classList.add('another-class');
+  }
 };
