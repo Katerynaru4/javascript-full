8a6ba3b1e434160cc45c948f6eb9f91e6268e436
Katerynaru4 <70796523+Katerynaru4@users.noreply.github.com>
2021-07-26 14:40:33 +0300

    Single handling of the 'click' event on the button

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector('.single-use-btn');
 const clickHandler = () => {
   console.log('clicked');
+  btn.removeEventListener('click', clickHandler);
 };
-btn.addEventListener('click', clickHandler, { once: true });
+btn.addEventListener('click', clickHandler);
