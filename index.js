c8628708156cac0c1a4d1f133f82599e21b61ad8
Katerynaru4 <70796523+Katerynaru4@users.noreply.github.com>
2021-07-27 16:58:18 +0300

    Form js

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
-const paginationPages = document.querySelectorAll('.pagination__page');
-const handleClick = (event) => {
-  console.log(event.target.dataset.pageNumber);
+const emailInputElem = document.querySelector('#email');
+const passwordInputElem = document.querySelector('#password');
+const emailErrorElem = document.querySelector('.error-text_email');
+const passwordErrorElem = document.querySelector('.error-text_password');
+const formElem = document.querySelector('.login-form');
+
+const isRequired = (value) => (value ? undefined : 'Required');
+const isEmail = (value) =>
+  value.includes('@') ? undefined : 'Should be an email';
+
+const validatorsByField = {
+  email: [isRequired, isEmail],
+  password: [isRequired],
 };
-[...paginationPages].forEach((button) =>
-  button.addEventListener('click', handleClick)
-);
+
+const validate = (fieldName, value) => {
+  const validators = validatorsByField[fieldName];
+  return validators
+    .map((validator) => validator(value))
+    .filter((error) => error)
+    .join(', ');
+};
+
+const onEmailChange = (event) => {
+  const errorText = validate('email', event.target.value);
+
+  emailErrorElem.textContent = errorText;
+};
+
+const onPasswordChange = (event) => {
+  const errorText = validate('password', event.target.value);
+
+  passwordErrorElem.textContent = errorText;
+};
+
+emailInputElem.addEventListener('input', onEmailChange);
+passwordInputElem.addEventListener('input', onPasswordChange);
+
+const onFormSubmit = (event) => {
+  event.preventDefault();
+  const formData = [...new FormData(formElem)].reduce(
+    (acc, [field, value]) => ({ ...acc, [field]: value }),
+    {}
+  );
+  alert(JSON.stringify(formData));
+};
+
+formElem.addEventListener('submit', onFormSubmit);
