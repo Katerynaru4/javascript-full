0bdb54c403cb2fd198b535744b7519f793492ae8
Katerynaru4 <70796523+Katerynaru4@users.noreply.github.com>
2021-08-12 01:10:38 +0300

    get best developers

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
-const showBtn = document.querySelector('.name-form__btn');
-const input = document.querySelector('.name-form__input');
+const obj1 = { days: 16, userId: 'Katerynaru4', repoId: 'javascript-full' };
+const obj2 = { days: 16, userId: 'Katerynaru4', repoId: 'calendar' };
+const obj3 = { days: 16, userId: 'Katerynaru4', repoId: 'calendar' };
 
-const avatar = document.querySelector('.user__avatar');
-const username = document.querySelector('.user__name');
-const userlocation = document.querySelector('.user__location');
+const getInfo = (commits, days) => {
+  const count = commits.filter(
+    (commit) =>
+      new Date() - new Date(commit.commit.author.date) < 86400000 * days
+  ).length;
+  const author = commits[0].commit.author;
+  const userName = author.name;
+  const userEmail = author.email;
 
-const fetchRequest = (userName) => {
-  fetch(`https://api.github.com/users/${userName}`)
-    .then((response) => response.json())
-    .then((response) => {
-      username.textContent = response.name;
-      userlocation.textContent = '' ? '' : `from ${response.location}`;
-      avatar.src = response.avatar_url;
-    });
+  return { count, name: userName, email: userEmail };
 };
 
-const getName = () => {
-  const userName = input.value;
-  fetchRequest(userName);
+const findMax = (arr) => {
+  const result = arr.reduce((prev, current) => {
+    if (+current.count > +prev.count) {
+      return current;
+    }
+    return prev;
+  });
+  return result;
 };
 
-showBtn.addEventListener('click', () => getName());
+async function getUserInfo(users) {
+  const results = await Promise.all(
+    users.map((user) =>
+      fetch(
+        `https://api.github.com/repos/${user.userId}/${user.repoId}/commits?per_page=100`
+      )
+        .then((response) => response.json())
+        .then((res) => getInfo(res, user.days))
+    )
+  );
+  const maxCount = findMax(results).count;
+  const res = results.filter((user) => user.count === maxCount);
+
+  return res;
+}
+
+export const getMostActiveDevs = (...users) =>
+  Promise.resolve(getUserInfo(users)).then((data) => console.log(data));
+
+getMostActiveDevs(obj1, obj2, obj3);
