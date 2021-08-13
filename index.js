ce994e8cffe52364f778bf30cd9e7a5fa81ad69e
Katerynaru4 <70796523+Katerynaru4@users.noreply.github.com>
2021-08-13 18:48:38 +0300

    Create user form

--- index.js
+++ index.js
@@ -1,65 +1,47 @@
-const baseUrl = 'https://61166547d98aef0017fe29cd.mockapi.io/users';
+const baseUrl = 'https://61166547d98aef0017fe29cd.mockapi.io/usersForm';
 
-export const getUsersList = () =>
-  fetch(baseUrl).then((response) => response.json());
+const submitBtnElem = document.querySelector('.submit-button');
+const loginFormElem = document.querySelector('.login-form');
+const InputFormElems = document.querySelectorAll('.form-input');
+const emailInputElem = document.querySelector('.form-input[name="email"]');
+const nameInputElem = document.querySelector('.form-input[name="name"]');
+const passwordInputElem = document.querySelector(
+  '.form-input[name="password"]'
+);
+const errorTextElem = document.querySelector('.error-text');
 
-export const getUserById = (userId) =>
-  fetch(`${baseUrl}/${userId}`).then((response) => response.json());
+const onSubmitForm = (e) => {
+  e.preventDefault();
 
-export const createUser = (userData) =>
+  const newUser = {
+    email: emailInputElem.value,
+    name: nameInputElem.value,
+    password: passwordInputElem.value,
+  };
+
   fetch(baseUrl, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json;charset=utf-8',
     },
-    body: JSON.stringify(userData),
-  }).then((response) => response.json());
+    body: JSON.stringify(newUser),
+  })
+    .then((response) => response.json())
+    .then((response) => alert(JSON.stringify(response)))
+    .then(() => {
+      [...InputFormElems].map((el) => {
+        el.value = '';
+      });
+    })
+    .catch(() => {
+      errorTextElem.textContent = new Error('Failed to create user');
+    });
+};
 
-export const deleteUser = (userId) =>
-  fetch(`${baseUrl}/${userId}`, { method: 'DELETE' }).then((response) =>
-    response.json()
-  );
+const onChangeInput = (e) => {
+  errorTextElem.textContent = '';
+  submitBtnElem.disabled = !loginFormElem.reportValidity();
+};
 
-export const updateUser = (userId, userData) =>
-  fetch(`${baseUrl}/${userId}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-    },
-    body: JSON.stringify(userData),
-  }).then((response) => response.json());
-
-// examples
-// getUsersList().then((users) => {
-//   console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
-// });
-
-// getUserById('2').then((userData) => {
-//   console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
-// });
-
-// const newUserData = {
-//   email: 'cool@email.com',
-//   firstName: 'Iron',
-//   lastName: 'Man',
-//   age: 42,
-// };
-
-// createUser(newUserData).then(() => {
-//   console.log('User created');
-// });
-
-// const updatedUserData = {
-//   email: 'new@email.com',
-//   firstName: 'John',
-//   lastName: 'Doe',
-//   age: 17,
-// };
-
-// updateUser('1', updatedUserData).then(() => {
-//   console.log('User updated');
-// });
-
-// deleteUser('2').then(() => {
-//   console.log('User updated');
-// });
+loginFormElem.addEventListener('submit', onSubmitForm);
+loginFormElem.addEventListener('input', onChangeInput);
