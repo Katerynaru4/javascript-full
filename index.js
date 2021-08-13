dbe50bfcf8c67422d21aa56ecbda109ffafe0ea7
Katerynaru4 <70796523+Katerynaru4@users.noreply.github.com>
2021-08-13 16:52:50 +0300

    requests

--- index.js
+++ index.js
@@ -1,45 +1,65 @@
-const obj1 = { days: 16, userId: 'Katerynaru4', repoId: 'javascript-full' };
-const obj2 = { days: 16, userId: 'Katerynaru4', repoId: 'calendar' };
-const obj3 = { days: 16, userId: 'Katerynaru4', repoId: 'calendar' };
+const baseUrl = 'https://61166547d98aef0017fe29cd.mockapi.io/users';
 
-const getInfo = (commits, days) => {
-  const count = commits.filter(
-    (commit) =>
-      new Date() - new Date(commit.commit.author.date) < 86400000 * days
-  ).length;
-  const author = commits[0].commit.author;
-  const userName = author.name;
-  const userEmail = author.email;
+export const getUsersList = () =>
+  fetch(baseUrl).then((response) => response.json());
 
-  return { count, name: userName, email: userEmail };
-};
+export const getUserById = (userId) =>
+  fetch(`${baseUrl}/${userId}`).then((response) => response.json());
 
-const findMax = (arr) => {
-  const result = arr.reduce((prev, current) => {
-    if (+current.count > +prev.count) {
-      return current;
-    }
-    return prev;
-  });
-  return result;
-};
+export const createUser = (userData) =>
+  fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(userData),
+  }).then((response) => response.json());
 
-async function getUserInfo(users) {
-  const results = await Promise.all(
-    users.map((user) =>
-      fetch(
-        `https://api.github.com/repos/${user.userId}/${user.repoId}/commits?per_page=100`
-      )
-        .then((response) => response.json())
-        .then((res) => getInfo(res, user.days))
-    )
+export const deleteUser = (userId) =>
+  fetch(`${baseUrl}/${userId}`, { method: 'DELETE' }).then((response) =>
+    response.json()
   );
-  const maxCount = findMax(results).count;
-  const res = results.filter((user) => user.count === maxCount);
 
-  return res;
-}
+export const updateUser = (userId, userData) =>
+  fetch(`${baseUrl}/${userId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(userData),
+  }).then((response) => response.json());
 
-export const getMostActiveDevs = (...users) => getUserInfo(users);
+// examples
+// getUsersList().then((users) => {
+//   console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
+// });
 
-getMostActiveDevs(obj1, obj2, obj3);
+// getUserById('2').then((userData) => {
+//   console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
+// });
+
+// const newUserData = {
+//   email: 'cool@email.com',
+//   firstName: 'Iron',
+//   lastName: 'Man',
+//   age: 42,
+// };
+
+// createUser(newUserData).then(() => {
+//   console.log('User created');
+// });
+
+// const updatedUserData = {
+//   email: 'new@email.com',
+//   firstName: 'John',
+//   lastName: 'Doe',
+//   age: 17,
+// };
+
+// updateUser('1', updatedUserData).then(() => {
+//   console.log('User updated');
+// });
+
+// deleteUser('2').then(() => {
+//   console.log('User updated');
+// });
